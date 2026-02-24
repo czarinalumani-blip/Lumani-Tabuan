a1b1113ba997d69c02a8adddae3f88920c88406c
unknown <hannahgtabuan@gmail.com>
2026-02-24 09:28:17 +0800

    (Lumani) Added Function

--- By_Pair.py
+++ By_Pair.py
@@ -1,4 +1,16 @@
 def greet(name):
     return "Hello, " + name
 
+print (" ")
+print ("Student 2: Tabuan")
 print(greet("Github"))
+
+
+
+def add(a,b): 
+    return a + b
+
+print(" ")
+print ("Student 2: Lumani")
+print (add(5,3))
+print (" ")
